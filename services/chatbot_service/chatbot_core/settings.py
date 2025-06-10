aacafafc55865fbb1f378e8a1da20f0b7c487218
HieuDT <dthieu2512@gmail.com>
2025-06-10 17:32:54 +0700

    fix: register

--- services/chatbot_service/chatbot_core/settings.py
+++ services/chatbot_service/chatbot_core/settings.py
@@ -86,8 +86,7 @@ WSGI_APPLICATION = 'chatbot_core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy',
     }
 }
 
